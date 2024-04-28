e04e60de167b65351ce1fa2a870e7fcfdaab4d6b
bneighbs6 <152036405+bneighbs6@users.noreply.github.com>
2024-04-27 19:21:44 -0600

    Implemented parentheses function

--- src/parentheses/parentheses.js
+++ src/parentheses/parentheses.js
@@ -1,5 +1,21 @@
 const Stack = require("../lib/stack");
 
-const match = (expression) => {};
+const match = (expression) => {
+    const newStack = new Stack(); 
+    for (let i = 0; i < expression.length; i++) {
+        if (expression[i] === "(") {
+            newStack.push("(");
+        } else {
+            if (expression[i] === ")") {
+                if (newStack.top) {
+                    newStack.pop();
+                } else {
+                    return false;
+                }
+            }
+        }
+    }
+    return !newStack.top;
+};
 
 module.exports = match;
